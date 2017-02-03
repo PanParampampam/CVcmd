3b3f4c8ad87a4819ccf555cc873f91bf0a5fc6d3
Kuba <giedrysjakub@gmail.com>
2017-02-03 16:44:47 +0100

    poprawa wyswietlania textarea

--- dodaj_info3zatwierdz.php
+++ dodaj_info3zatwierdz.php
@@ -47,7 +47,7 @@
 		
 		<?php
 		echo "Treść: <strong>" . $_SESSION['naglowek'] . "</strong></br></br>";
-		echo '<div style="white-space: pre-line; margin-left: 40px;">' . $_SESSION['info'] . "</br></br></div>";
+		echo '<div style="white-space: pre-line; margin-left: 40px; width: 205mm">' . $_SESSION['info'] . "</br></br></div>";
 		if(isset($_SESSION['error_info'])) {
 			echo $_SESSION['error_info'];
 			unset($_SESSION['error_info']);
