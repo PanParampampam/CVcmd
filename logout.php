4a957a139554ab75c597b1078ddd249be943ac6d
Kuba <giedrysjakub@gmail.com>
2017-03-06 16:35:42 +0100

    brak bledu przy niezaladowanych plikach

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 ﻿<?php
 	session_start();
-	unlink($_SESSION['usun_zdjecie']);
-	unlink($_SESSION['usun_cv']);
+	if (isset($_SESSION['usun_zdjecie'])) unlink($_SESSION['usun_zdjecie']);
+	if (isset($_SESSION['usun_cv'])) unlink($_SESSION['usun_cv']);
 	session_unset();
 	$_SESSION['info']='CVcmd:\&gt;logout</br><span style=color:green>Wylogowano</span></br></br>';
 	header('Location: index.php');
--- session_timeout.php
+++ session_timeout.php
@@ -2,8 +2,8 @@
 	$time = $_SERVER["REQUEST_TIME"];
 	$timeout_duration = 1800;
 	if (isset($_SESSION["LAST_ACTIVITY"]) && ($time - $_SESSION["LAST_ACTIVITY"]) > $timeout_duration) {
-		unlink($_SESSION['usun_zdjecie']);
-		unlink($_SESSION['usun_cv']);
+		if (isset($_SESSION['usun_zdjecie'])) unlink($_SESSION['usun_zdjecie']);
+		if (isset($_SESSION['usun_cv'])) unlink($_SESSION['usun_cv']);
 		session_unset();     
 		session_destroy();
 		header('Location: index.php');  
